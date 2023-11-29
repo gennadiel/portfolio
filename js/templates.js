ad82cdaef33d0c0ffaa85ebfdf74d6b40a0fe6e2
viktorahlberk <viktorahlberk@gmail.com>
2023-11-29 18:41:58 +0200

    templates

---
+++ js/templates.js
@@ -0,0 +1,23 @@
+export const headerTemplate = `<div id="headerbar">
+        <span id="logo"><a href="/index.html">Геннадий Ельцов</a></span>
+        <div id="linksbar">
+            <a href="/pages/bio.html">
+                <div id="home">
+                    <p>Биография</p>
+                </div>
+            </a>
+            <a href="/pages/publications.html">
+                <div id="home">
+                    <p>Статьи</p>
+                </div>
+            </a>
+            <a href="/pages/gallery.html">
+                <div id="home">
+                    <p>Галерея</p>
+                </div>
+            </a>
+        </div>
+        <div>
+            <img id="key" src="/style/img/key_FILL0_wght400_GRAD0_opsz24.png" alt="key">
+        </div>
+    </div>`;
